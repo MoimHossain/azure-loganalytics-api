af44a10e7b97a77c712a3ec8571b23bb3393d877
Rene van Osnabrugge <rvanosnabrugge@live.com>
2018-10-25 11:18:14 +0200

    Added method for posting to Log Analytics

--- LogAnalytics/Controllers/LogsController.cs
+++ LogAnalytics/Controllers/LogsController.cs
@@ -3,6 +3,9 @@
 using LogAnalytics.Models;
 using LogAnalytics.Supports;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Threading.Tasks;
 
 namespace LogAnalytics.Controllers
@@ -21,6 +24,7 @@ namespace LogAnalytics.Controllers
         private readonly string ClientID;
         private readonly string ClientSecret;
         private readonly string LogAnalyticsWorkspaceID;
+        private readonly string LogAnalyticsPrimaryKey;
 
         public LogsController()
         {
@@ -31,10 +35,13 @@ namespace LogAnalytics.Controllers
             ClientID = Env.GetEnvironmentVariable(Env.Keys.ClientID);
             ClientSecret = Env.GetEnvironmentVariable(Env.Keys.ClientSecret);
             LogAnalyticsWorkspaceID = Env.GetEnvironmentVariable(Env.Keys.LogAnalyticsWorkspaceID);
-            
+            LogAnalyticsPrimaryKey = Env.GetEnvironmentVariable(Env.Keys.LogAnalyticsPrimaryKey);
+
+
             uri = $"https://api.loganalytics.io/v1/workspaces/{this.LogAnalyticsWorkspaceID}/query";
             this.httpFactory = new HttpClientFactory(new TokenHelper(this.TenantId, this.ClientID, this.ClientSecret, this.LogAnalyticsResource));
         }
+
 
         [HttpGet("")]
         public async Task<ActionResult<Logs>> GetLogsAsync(string query, string timespan)
@@ -49,6 +56,27 @@ namespace LogAnalytics.Controllers
                 Timespan = timespan
             });
             return logs;
+        }
+
+        [HttpPost("")]
+        public async Task<ActionResult> PostCustomLogType([FromBody]JObject body)
+        {
+            if (!Request.HasValidApiKey(SharedAccessSignature))
+            {
+                return new UnauthorizedResult();
+            }
+
+            var customlogType = body.GetValue("customlogType").Value<string>();
+            var json = body.GetValue("json").ToString(Formatting.None);
+
+            if (!string.IsNullOrEmpty(json))
+            {
+
+                var logAnalytics = new LogAnalyticsHelper(LogAnalyticsWorkspaceID, LogAnalyticsPrimaryKey);
+                await logAnalytics.AddCustomLogJsonAsync(customlogType, json, "");
+                return new OkResult();
+            }
+            return new UnprocessableEntityResult();
         }
     }
 }
